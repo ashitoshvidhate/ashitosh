a1115000a724dd0786e038dc7ea45ae0c8ce3aac
ashitoshvidhate <ashitoshvidhate45@gmail.com>
2016-10-14 15:55:38 +0530

    Add files via upload

---
+++ bday.py
@@ -0,0 +1,13 @@
+import time
+def calltime():
+    i=0 
+    a=int(input("Enter a date:"))
+    b=int(input("enter a month:"))
+    c=int(input("Enter a year:"))
+    localtime=time.localtime()
+    print('day of month')
+    print('day of week')
+    print('day of year')
+calltime()
+    
+          
